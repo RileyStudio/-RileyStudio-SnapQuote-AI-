94214c3c5788815aad247b9a2a3cb7f728cfbfe2
RileyStudio <rileydigitalstudio@gmail.com>
2026-06-25 01:31:35 -0500

    Delete components directory

--- components/components/SectionLabel.jsx
+++
@@ -1,7 +0,0 @@
-export default function SectionLabel({ children }) {
-  return (
-    <p className="font-display font-semibold text-xs uppercase tracking-widest text-ink/50 mb-3">
-      {children}
-    </p>
-  );
-}
